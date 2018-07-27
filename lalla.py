f0ef3d0fe7805f721f535dff7a0a0c83a1b8ad94
Emiyaaaaa <2914034404@qq.com>
2018-07-27 16:38:41 +0800

    1

--- lalla.py
+++
@@ -1,7 +0,0 @@
-# _*_ coding: utf-8 _*_
-__author__ = 'zy'
-__date__ = '2018/7/27 11:26'
-
-
-print("hello")
-print ("hello")
--- test2.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Author  : Li Haozheng
-# @Time    : 2018/7/27 11:16
